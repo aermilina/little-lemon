64a1a5df0557f5590ba551b39eb49f4348f68d09
aermilina <anna.ermilina@gmail.com>
2023-11-14 15:05:26 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
 import './App.css';
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer"
 
 function App() {
   return (
-    <div className="App">
-      i'm app
-    </div>
+    <React.Fragment className="App">
+        <Header/>
+        <Main/>
+        <Footer/>
+    </React.Fragment>
   );
 }
 
